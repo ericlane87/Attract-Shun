74a8de2fa04d6950ef2ea6acfe8d652cbaa64fbf
Eric Lane <ericlane87@gmail.com>
2026-04-04 12:23:20 -0400

    Centralize asset versioning across pages

---
+++ version.js
@@ -0,0 +1,37 @@
+(function () {
+  const APP_VERSION = "20260404-4";
+
+  function versionedPath(path) {
+    return `${path}?v=${APP_VERSION}`;
+  }
+
+  function loadStyle(path) {
+    const link = document.createElement("link");
+    link.rel = "stylesheet";
+    link.href = versionedPath(path);
+    document.head.appendChild(link);
+  }
+
+  function loadScript(path) {
+    return new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.src = versionedPath(path);
+      script.onload = resolve;
+      script.onerror = reject;
+      document.body.appendChild(script);
+    });
+  }
+
+  function loadScripts(paths) {
+    return paths.reduce((chain, path) => {
+      return chain.then(() => loadScript(path));
+    }, Promise.resolve());
+  }
+
+  window.AppAssets = {
+    version: APP_VERSION,
+    loadStyle,
+    loadScripts,
+    versionedPath,
+  };
+})();
